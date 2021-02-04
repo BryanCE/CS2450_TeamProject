db622e71a0e129d980be7801a6902ee1eb9319db
Valina16 <78060066+Valina16@users.noreply.github.com>
2021-02-03 19:33:46 -0700

    Add files via upload

---
+++ Memory.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Memory {
+	private:
+		int memory[100] = { 0 };
+	public:
+		//takes an integer in range 0-100 (excluding 100) for the address, uses it as the index in array at which to store value
+		void StoreValue(int address, int value);
+
+		//takes an integer in range 0-100 for the address and returns value at that index of array
+		int LoadFromMemory(int address);
+
+		//displays all values stored in memory or 0 if empty in the form of a table
+		void DisplayMemory();
+};
