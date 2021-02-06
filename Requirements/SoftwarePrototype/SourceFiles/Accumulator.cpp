c964c20d0d1acfb720ec97d714e4cdfc28dee68b
tyler961 <37886566+tyler961@users.noreply.github.com>
2021-02-05 23:21:35 -0700

    Added Jon's Accumulator code

--- Requirements/SoftwarePrototype/SourceFiles/Accumulator.cpp
+++ Requirements/SoftwarePrototype/SourceFiles/Accumulator.cpp
@@ -86,6 +86,8 @@ void Accumulator::LoadInstruction(int addr)
 	mOperationCode = num / divisor;
 	// Last two numbers
 	mOperand = num % divisor;
+	
+	mInstructionCounter++;
 
 	return;
 }
@@ -134,28 +136,105 @@ void Accumulator::Add(int addr)
 
 void Accumulator::Subtract()
 {
+	mRegister -= mMemory->LoadFromMemory(mOperand);
+    	return;
 }
 
 void Accumulator::Divide()
 {
+	// If Operand is 0, if statement will catch it
+	if (mMemory->LoadFromMemory(mOperand) == 0) 
+	{
+        	cout << "Attempt to divide by zero";
+        	exit(1);
+    	}
+	mRegister = mRegister/mMemory->LoadFromMemory(mOperand);
+    	return;
 }
 
 void Accumulator::Multiply()
 {
+	mRegister *= mMemory->LoadFromMemory(mOperand);
+    	return;
 }
 
 void Accumulator::Branch()
 {
+	mInstructionMemoryAddress = mOperand;
+    	return;
 }
 
 void Accumulator::BranchNeg()
 {
+	if (mRegister < 0) 
+        	mInstructionMemoryAddress = mOperand;
+	
+    	return;
 }
 
 void Accumulator::BranchZero()
 {
+	if (mRegister == 0) 
+        	mInstructionMemoryAddress = mOperand;
+
+    return;
 }
 
 void Accumulator::DisplayAccumulator()
 {
+	// Displays the register, instructionCounter, instructionRegister, operationCode and operand variables. 
+    cout << "\nREGISTERS: " << endl;
+    
+    cout << "Accumulator\t";
+    //if accumulator value is 1 digit
+    if (mRegister < 10){
+        cout << "000";
+    }
+    //if accumulator value is 2 digits
+    else if(mRegister < 100 && mRegister > 9){
+        cout << "00";
+    }
+    //if accumulator value is 3 digits
+    else if(mRegister <= 999 && mRegister > 99){
+        cout << "0";
+    }
+    //calls out accumulator value
+    cout << mRegister << endl;
+    
+    cout << "InstructionCounter\t";
+    //if instructionCounter is 1 digit
+    if (mInstructionCounter < 10)
+        cout << "0";
+    //calling the value of instructionCounter
+    cout << mInstructionCounter << endl;
+    
+    cout << "InstructionRegister\t";
+    //if instructionRegister value is 1 digit
+    if (mInstructionRegister < 10){
+        cout << "000";
+    }
+    //if instructionRegister value is 2 digits
+    else if(mInstructionRegister < 100 && mInstructionRegister > 9){
+        cout << "00";
+    }
+    //if instructionRegister value is 3 digits
+    else if(mInstructionRegister <= 999 && mInstructionRegister > 99){
+        cout << "0";
+    }
+    //calls out the value of instructionRegister
+    cout << mInstructionRegister << endl;
+    
+    cout << "OperationCode \t";
+    //if operationcode is in 1 digit
+    if (mOperationCode < 10)
+        cout << "0";
+    //calls out operationcode
+    cout << mOperationCode << endl;
+    
+    cout << "Operand \t";
+    //if operand value is 1 digit
+    if (mOperand < 10)
+        cout << "0";
+    //calls out operand value
+    cout << mOperand << endl;
 }
