b61e89b98b551254080976aee0704249f5dc0209
tyler961 <37886566+tyler961@users.noreply.github.com>
2021-04-11 09:49:13 -0600

    Update RunMath.h

--- Requirements/Milestone3/SoftwarePrototype/Headers/RunMath.h
+++ Requirements/Milestone3/SoftwarePrototype/Headers/RunMath.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Model.h"
-//coded by Bryan Edman
+
 class RunMath
 {
 private:
@@ -12,18 +12,18 @@ public:
 
 	// Operation Code: 30	   Calls LoadFromMemory() in class Memory. 
 // Then takes that value and the value in the register and adds them. This new value OVERWRITES the register variable.
-	int Add(int addr, int regVal);
+	int Add(int addr, int regVal, int &regOverflow);
 
 	// Operation Code: 31	   Calls LoadFromMemory() in class Memory. Then takes that value and the 
 	// value in the register and subtracts them. This new value OVERWRITES the register variable.
-	int Subtract(int addr, int regVal);
+	int Subtract(int addr, int regVal, int &regOverflow);
 
 	// Operation Code: 32	   Calls LoadFromMemory() in class Memory. Order here is important. 
 	// Divide the value in the accumulator from the value from the memory. This new value OVERWRITES the register variable.
-	int Divide(int addr, int regVal);
+	int Divide(int addr, int regVal, int &regOverflow);
 
 	// Operation Code: 33	   Calls LoadFromMemory() in class Memory. 
 	// Then takes that value and the value in the register and multiplies them. This new value OVERWRITES the register variable.
-	int Multiply(int addr, int regVal);
+	int Multiply(int addr, int regVal, int &regOverflow);
 };
 
