686ccbbc978b60fe258fdcd36b06f971fb5fba59
tyler961 <37886566+tyler961@users.noreply.github.com>
2021-04-11 09:50:04 -0600

    Update RunMath.cpp

--- Requirements/Milestone3/SoftwarePrototype/Source/RunMath.cpp
+++ Requirements/Milestone3/SoftwarePrototype/Source/RunMath.cpp
@@ -1,7 +1,7 @@
 #include "RunMath.h"
 #include <stdlib.h>
 #include <iostream>
-//coded by Bryan Edman
+
 void RunMath::SetModel(Model* mdl)
 {
 	mMemory = mdl;
@@ -12,35 +12,161 @@ Model* RunMath::GetModel()
 	return mMemory;
 }
 
-int RunMath::Add(int addr, int regVal)
+int RunMath::Add(int addr, int regVal, int& regOverflow)
 {
-	//get actual number from memory
-	int number = mMemory->LoadFromMemory(addr);
-	//add whats in the register to the operands data and keep it in the accumulator register
-	return regVal + number;
-}
+	int regSndHalf = regOverflow;
+	int memSndHalf = 0;
+	if (mMemory->isSixDigits[addr])
+		memSndHalf = mMemory->LoadFromMemory(addr + 1);
 
-int RunMath::Subtract(int addr, int regVal)
-{
-	//get actual number from memory
-	int number = mMemory->LoadFromMemory(addr);
-	return number - regVal;
-}
 
-int RunMath::Divide(int addr, int regVal)
-{
-	// If Operand is 0, if statement will catch it
-	int num = mMemory->LoadFromMemory(addr);
-	if (num == 0)
+	int regFstHalf = regVal;
+	int memFstHalf = mMemory->LoadFromMemory(addr);
+	
+	int regTotal = regFstHalf;
+	if(regOverflow != 0)
+		regTotal = (regFstHalf * 1000) + regSndHalf;
+
+	int memTotal = memFstHalf;
+	if(mMemory->isSixDigits[addr])
+		memTotal = (memFstHalf * 1000) + memSndHalf;
+
+	int total = regTotal + memTotal;
+
+	// Check if its overflow
+	if (total > 999999)
 	{
-		std::cout << "Attempt to divide by zero";
+		cout << "Error. Overflow on register addition with address " << addr << endl;
+		total = total - 1000000;
+	}
+
+	// Now have to resplit the number if its over 4 digits
+	if (total > 9999)
+	{
+		int secondHalf = total % 1000;
+		int firstHalf = total / 1000;
+		// Store the value back into the reg
+		regOverflow = secondHalf;
+		return firstHalf;
+	}
+	else
+		return total;
+	
+}
+
+int RunMath::Subtract(int addr, int regVal, int &regOverflow)
+{
+	int regSndHalf = regOverflow;
+	int memSndHalf = 0;
+	if (mMemory->isSixDigits[addr])
+		memSndHalf = mMemory->LoadFromMemory(addr + 1);
+
+	// Now the first half
+	int regFstHalf = regVal;
+	int memFstHalf = mMemory->LoadFromMemory(addr);
+	
+	int regTotal = regFstHalf;
+	if (regOverflow != 0)
+		regTotal = (regFstHalf * 1000) + regSndHalf;
+
+	int memTotal = memFstHalf;
+	if (mMemory->isSixDigits[addr])
+		memTotal = (memFstHalf * 1000) + memSndHalf;
+
+	int total = memTotal - regTotal;
+
+	// Now have to resplit the number if its over 4 digits
+	if (total > 9999)
+	{
+		int secondHalf = total % 1000;
+		int firstHalf = total / 1000;
+		// Store the value back into the reg
+		regOverflow = secondHalf;
+		return firstHalf;
+	}
+	else
+		return total;
+	
+}
+
+int RunMath::Divide(int addr, int regVal, int &regOverflow)
+{
+	int memFstHalf = mMemory->LoadFromMemory(addr);
+	int regFstHalf = regVal;
+
+	int memSndHalf = 0;
+	int regSndHalf = regOverflow;
+	if (mMemory->isSixDigits[addr])
+		memSndHalf = mMemory->LoadFromMemory(addr + 1);
+
+	if (memFstHalf == 0)
+	{
+		cout << "Attempt to divide by zero";
 		exit(1);
 	}
-	return regVal/num;
+
+	int regTotal = regFstHalf;
+	if (regOverflow != 0)
+		regTotal = (regFstHalf * 1000) + regSndHalf;
+
+	int memTotal = memFstHalf;
+	if (mMemory->isSixDigits[addr])
+		memTotal = (memFstHalf * 1000) + memSndHalf;
+
+	int total = regTotal / memTotal;
+
+	// Now have to resplit the number if its over 4 digits
+	if (total > 9999)
+	{
+		int secondHalf = total % 1000;
+		int firstHalf = total / 1000;
+		// Store the value back into the reg
+		regOverflow = secondHalf;
+		return firstHalf;
+	}
+	else
+		return total;
 }
 
-int RunMath::Multiply(int addr, int regVal)
+int RunMath::Multiply(int addr, int regVal, int &regOverflow)
 {
-	int num = mMemory->LoadFromMemory(addr);
-	return num * regVal;
+	int memFstHalf = mMemory->LoadFromMemory(addr);
+	int regFstHalf = regVal;
+
+	int memSndHalf = 0;
+	int regSndHalf = regOverflow;
+	if (mMemory->isSixDigits[addr])
+		memSndHalf = mMemory->LoadFromMemory(addr + 1);
+
+	int regTotal = regFstHalf;
+	if (regOverflow != 0)
+		regTotal = (regFstHalf * 1000) + regSndHalf;
+
+	int memTotal = memFstHalf;
+	if (mMemory->isSixDigits[addr])
+		memTotal = (memFstHalf * 1000) + memSndHalf;
+
+	int total = regTotal * memTotal;
+
+	// Check if its overflow
+	if (total > 999999)
+	{
+		cout << "Error. Overflow on register multiplication with address " << addr << endl;
+		int reduction = total / 1000000;
+		int correctedNum = total - (reduction * 1000000);
+
+		total = correctedNum;
+	}
+
+	// Resplit if above 4 digits
+	if (total > 9999)
+	{
+		int secondHalf = total % 1000;
+		int firstHalf = total / 1000;
+
+		regOverflow = secondHalf;
+		return firstHalf;
+	}
+	else
+		return total;
 }
